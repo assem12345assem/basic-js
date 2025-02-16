60a68538218558c6e1a4e19338deae030624724a
Asem Asfandiarova <assemsim@gmail.com>
2025-02-16 13:13:17 +0600

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
+const util = require('util');
 /**
  * Extract season from given date and expose the enemy scout!
  * 
@@ -11,11 +10,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason( date ) {
+  if(date === null || date === undefined) return "Unable to determine the time of year!";
+  if(!isValid( date ) ) throw new Error("Invalid date!");
+  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+  const index = Math.trunc((date.getMonth() + 1) / 3) % 4;
+  return seasons[index];
 }
-
+function isValid(date) {
+  return util.types.isDate(date);
+}
 module.exports = {
   getSeason
 };
