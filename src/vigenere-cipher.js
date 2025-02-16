7ed55c0c14a9b1f5e6216e3ab402e43d497c258b
Asem Asfandiarova <assemsim@gmail.com>
2025-02-16 21:14:22 +0600

    feat: vignere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  isReverse;
+  aCode = 'A'.charCodeAt();
+  zCode = 'Z'.charCodeAt();
+  constructor(value) {
+    this.isReverse = value === false;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    return this.helper(message, key, this.encryptHelper.bind(this));
   }
+  decrypt(message, key) {
+    return this.helper(message, key, this.decryptHelper.bind(this));
+  }
+  helper (message, key, strategy) {
+    if(!message || !key) throw new Error('Incorrect arguments!')
+    const msg = message.toUpperCase();
+    const keyDist = key.toUpperCase().split('').map((c) => c.charCodeAt() - this.aCode);
+    const n = msg.length;
+    const m = keyDist.length;
+    const ans = new Array(n);
+    for (let i = 0, j = 0; i < n; i++) {
+      const char = msg.charAt(i);
+      const charCode = char.charCodeAt();
+      if(charCode < this.aCode || charCode > this.zCode) {
+        ans[i] = char;
+      } else {
+        const distance = keyDist[j];
+        ans[i] = strategy(charCode, distance);
+        j++;
+        if(j >= m) j = 0;
+      }
+
+    }
+    if(this.isReverse) ans.reverse();
+    return ans.join('');
+  }
+  encryptHelper(charCode, distance) {
+    const n = charCode + distance;
+    return String.fromCharCode(n <= this.zCode ? n : this.aCode + n - this.zCode - 1);
+  }
+  decryptHelper(charCode, distance) {
+    const n = charCode - distance;
+    return String.fromCharCode( n >= this.aCode ? n : this.zCode - (this.aCode - n) + 1);
+  }
+
 }
 
 module.exports = {
